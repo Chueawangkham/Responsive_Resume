cc219d721b2ec74e6f5aa972a727af3c4da9e4a3
Chueawangkham <thibess091@gmail.com>
2022-07-14 22:22:58 +0700

    bass

--- lib/models/Project.dart
+++ lib/models/Project.dart
@@ -11,7 +11,7 @@ List<Project> demo_projects = [
         "On Flutter V2.* web officially supported on a stable branch. Today I share an Admin panel or you can call it dashboard UI build with flutter. Now you can build your app dashboard using flutter. This dashboard contains almost everything that you need like a chart, table, nice small card for showing info.",
   ),
   Project(
-    title: "Instagram Clone - Flutter UI",
+    title: "Instagram Clone - Flutter UI bass",
     description:
         "In the first part of our complete e-commerce app, we show you how you can create a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
   ),
